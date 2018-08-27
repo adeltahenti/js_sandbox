121d9b3513fcca91e2d8de227fd1cd068ccf14c4
adeltahenti <adel.tahenti@citycable.ch>
2018-08-27 10:31:36 +0200

    JS Prototypes Explained

--- app.js
+++ app.js
@@ -52,3 +52,56 @@ const re2 = new RegExp('\\w+');
 
 console.log(re2); 
 */
+
+/**** Prototypes Explained ****/
+/* 
+// Object.prototype
+// Person.prototype
+
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthday = new Date(dob);
+
+  // this.calculateAge = function () {
+  //   const diff = Date.now() - this.birthday.getTime();
+  //   const ageDate = new Date(diff);
+  //   return Math.abs(ageDate.getUTCFullYear() - 1970);
+  // }
+}
+
+// Calculate age
+Person.prototype.calculateAge = function () {
+  const diff = Date.now() - this.birthday.getTime();
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+// Get full name
+Person.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+}
+
+// Gets Maried
+Person.prototype.getMaried = function (newLastName) {
+  this.lastName = newLastName;
+}
+
+const john = new Person('John', 'Doe', '8-12-90');
+const mary = new Person('Mary', 'Johnson', 'March 20 1976');
+
+console.log(mary);
+
+console.log(john.calculateAge());
+
+console.log(mary.getFullName());
+
+mary.getMaried('Smith');
+
+console.log(mary.getFullName());
+
+console.log(mary.hasOwnProperty('firstName'));
+console.log(mary.hasOwnProperty('getFullName')); 
+*/
+
+/**** Prototypal Inheritance ****/
