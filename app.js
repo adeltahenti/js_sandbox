ca8d564c5c21c15ebb7b766ee0bcfaec18899867
adeltahenti <adel.tahenti@citycable.ch>
2018-08-28 08:19:49 +0200

    ES6 Classes

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ console.log(customer1.greeting());
 */
 
 /**** Using Object.create ****/
-
+/* 
 const personPrototypes = {
   greeting: function () {
     return `Hello there ${this.firstName} ${this.lastName}`;
@@ -181,4 +181,41 @@ const adel = Object.create(personPrototypes, {
 });
 
 console.log(adel);
-console.log(adel.greeting());
+console.log(adel.greeting()); 
+*/
+
+/**** ES6 Classes ****/
+
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
+
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
+
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  getMaried(newLastName) {
+    this.lastName = newLastName;
+  }
+
+  static addNumbers(x, y) {
+    return x + y;
+  }
+}
+
+const mary = new Person('Mary', 'Thomson', '11-13-1980');
+
+mary.getMaried('Williams');
+
+console.log(mary);
+
+console.log(Person.addNumbers(1, 4));
