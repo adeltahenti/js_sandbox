f7002819cf0407100425cedecca88cb50de8ee5e
adeltahenti <adel.tahenti@citycable.ch>
2018-08-28 09:14:41 +0200

    Sub Classes

--- app.js
+++ app.js
@@ -185,7 +185,7 @@ console.log(adel.greeting());
 */
 
 /**** ES6 Classes ****/
-
+/* 
 class Person {
   constructor(firstName, lastName, dob) {
     this.firstName = firstName;
@@ -218,4 +218,35 @@ mary.getMaried('Williams');
 
 console.log(mary);
 
-console.log(Person.addNumbers(1, 4));
+console.log(Person.addNumbers(1, 4)); 
+*/
+
+/**** Sub Classes ****/
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
+}
+
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName);
+    this.phone = phone;
+    this.membership = membership;
+  }
+
+  static getMembershipCost() {
+    return 500;
+  }
+}
+
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
+
+console.log(john.greeting());
+
+console.log(Customer.getMembershipCost());
